426d2fa1b3463c8c61aa4e8a9201cbfabaf58b08
Tintin <33134900+SamrathPalSingh@users.noreply.github.com>
2020-01-21 02:53:23 +0530

    Add files via upload

---
+++ src/screens/PlaybackScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const PlaybackScreen = () => {
+  return <Text style={{ fontSize: 48 }}>Playback Screen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default PlaybackScreen;
